1910bcd062529800d0d729d21b550a323a4db4a6
Berat Kir <beratkir61@gmail.com>
2022-06-10 00:09:03 +0200

    changed price to use most user-friendly notation

--- configuration.js
+++ configuration.js
@@ -1,4 +1,4 @@
-const { BigNumber } = require("ethers");
+const { BigNumber, ethers } = require("ethers");
 require("dotenv").config({ path: "options" });
 
 env = process.env;
@@ -6,7 +6,7 @@ env = process.env;
 const contractName = `${env.contractName}`;
 const name = `${env.name}`;
 const symbol = `${env.symbol}`;
-const price = BigNumber.from(`${env.price}`);
+const price = BigNumber.from(ethers.utils.parseEther(`${env.price}`));
 const maxMintPerTx = BigNumber.from(`${env.maxMintPerTx}`);
 const collectionSize = BigNumber.from(`${env.collectionSize}`);
 const maxFree = BigNumber.from(`${env.maxFree}`);
